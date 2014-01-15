681f613c37641fcc33820d56a31addebe4ce11d6
Adam Lundrigan <adamlundrigan@cdli.ca>
2014-01-15 12:50:46 -0330

    Update fixture to maintain compatibility with PHP < 5.4

--- tests/DoctrineDataFixtureTest/TestAsset/Fixtures/HasSL/FixtureA.php
+++ tests/DoctrineDataFixtureTest/TestAsset/Fixtures/HasSL/FixtureA.php
@@ -4,13 +4,40 @@ namespace DoctrineDataFixtureTest\TestAsset\Fixtures\HasSL;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class FixtureA implements FixtureInterface, ServiceLocatorAwareInterface
 {
-    use ServiceLocatorAwareTrait;
+    /**
+     * @var ServiceLocatorInterface
+     */
+    protected $serviceLocator = null;
+
 
     public function load(ObjectManager $manager)
     {
+    }
+    
+    /**
+     * Set service locator
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+    
+        return $this;
+    }
+    
+    /**
+     * Get service locator
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
     }
 }
